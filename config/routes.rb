a269f6c5c5920cdc734edb54806e30b9fd1afd43
wuyepabdul <wuyepabdul@gmail.com>
2022-02-23 23:01:32 +0100

    Deploy app

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
       get 'greetings/index'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
