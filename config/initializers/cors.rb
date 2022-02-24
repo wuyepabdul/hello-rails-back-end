7f65953d0ab498dd3129c8f042d55b060dd1d5ac
wuyepabdul <wuyepabdul@gmail.com>
2022-02-24 11:33:28 +0100

    Add frontend endpoint to cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000, https://duls-react-rails-frontend.herokuapp.com/"
+    origins "https://duls-react-rails-frontend.herokuapp.com"
 
     resource "*",
       headers: :any,
